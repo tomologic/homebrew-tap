daf23358317d74b660a6fc7ccb68813a2dc8aca2
Carl Loa Odin <loa@tomologic.com>
2015-10-15 07:47:37 +0200

    Update Wrench v0.27.0

--- wrench.rb
+++ wrench.rb
@@ -1,12 +1,12 @@
 class Wrench < Formula
   desc "Wrench build / test util"
   homepage "https://github.com/tomologic/wrench"
-  url "https://github.com/tomologic/wrench/releases/download/v0.26.0/wrench-v0.26.0-darwin-amd64"
-  version "0.26.0"
-  sha256 "6ea149d8ee154e8926e4f15edc6c6e711a907d29ae6cadb1be478b701292e1a4"
+  url "https://github.com/tomologic/wrench/releases/download/v0.27.0/wrench-v0.27.0-darwin-amd64"
+  version "0.27.0"
+  sha256 "ace1e5ef83f74eda3126187a306017c9e6bb80ed9499dbf7dd8765c597ae5878"
 
   def install
-    bin.install "wrench-v0.26.0-darwin-amd64"
-    mv bin/'wrench-v0.26.0-darwin-amd64', bin/'wrench'
+    bin.install "wrench-v0.27.0-darwin-amd64"
+    mv bin/'wrench-v0.27.0-darwin-amd64', bin/'wrench'
   end
 end
