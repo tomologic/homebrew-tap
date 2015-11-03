2d3c34498f68f8b156891851a629adbb25234658
Carl Loa Odin <loa@tomologic.com>
2015-11-03 13:49:05 +0100

    Add travis-ci-bumpminor.sh
    Script for adding semver tags to HEAD if missing.

--- semver-sh.rb
+++ semver-sh.rb
@@ -1,11 +1,11 @@
 class SemverSh < Formula
   desc 'Semver.sh'
-  homepage 'https://github.com/tomologic/utils'
+  homepage 'https://github.com/tomologic/utilities'
   version '0.1.1'
   sha256 'b8d181ff92b0e07cf0a149f6ace8556c89de0f58f563a791adaeb0ae28a2ee74'
   url 'https://raw.githubusercontent.com/tomologic/utilities/00773c99e4a75cb8f2f98ee895b1b522317b7cd6/semver.sh'
 
   def install
-    bin.install "semver.sh"
+    bin.install 'semver.sh'
   end
 end
---
+++ travis-ci-bumpminor-sh.rb
@@ -0,0 +1,11 @@
+class TravisCiBumpminorSh < Formula
+  desc 'travis-ci-bumpminor.sh'
+  homepage 'https://github.com/tomologic/utilities'
+  version '0.1.0'
+  sha256 '9626d60ac5fbe390d9a623c430378fd3e81264bd07338e292093446443766774'
+  url 'https://raw.githubusercontent.com/tomologic/utilities/36a28f43ef364d66007c2314dde9fa9c48deb52f/travis-ci-bumpminor.sh'
+
+  def install
+    bin.install 'travis-ci-bumpminor.sh'
+  end
+end
