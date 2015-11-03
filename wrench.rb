32842cb0857f65d98cd73db38bac83d5f4e3cc43
Carl Loa Odin <loa@tomologic.com>
2015-11-03 09:37:41 +0100

    Refactor wrench formula, remove class variables
    Class variables in Ruby works in unexpected ways. Parents inherit class
    variables from it's children thus contaminating all Formulas with risk
    of collision if used.

--- wrench.rb
+++ wrench.rb
@@ -4,17 +4,17 @@ class Wrench < Formula
   version '0.34.0'
 
   if OS.mac?
-    @@os = 'darwin'
+    os = 'darwin'
     sha256 '1c791043f85cb4121643c6c5ef8ca9d6989ba44fcdccb16cbaeff1d1f71389a1'
   elsif OS.linux?
-    @@os = 'linux'
+    os = 'linux'
     sha256 '7642112ba1d91c07de5d888a54fe418ee080c093550c63824afab411279b1d4d'
   end
 
-  url "https://github.com/tomologic/wrench/releases/download/v#{version}/wrench-#{version}-#{@@os}-amd64"
+  url "https://github.com/tomologic/wrench/releases/download/v#{version}/wrench-#{version}-#{os}-amd64"
 
   def install
-    bin.install "wrench-#{version}-#{@@os}-amd64"
-    mv bin/"wrench-#{version}-#{@@os}-amd64", bin/'wrench'
+    mv Dir.glob("wrench-*-amd64").first, 'wrench'
+    bin.install 'wrench'
   end
 end
