5e0d617c75a73a5e915ea523799ef6dcde57d314
Carl Loa Odin <loa@tomologic.com>
2015-11-03 14:26:15 +0100

    Update travis-ci-bumpminor.sh

--- travis-ci-bumpminor-sh.rb
+++ travis-ci-bumpminor-sh.rb
@@ -1,9 +1,11 @@
 class TravisCiBumpminorSh < Formula
   desc 'travis-ci-bumpminor.sh'
   homepage 'https://github.com/tomologic/utilities'
-  version '0.1.1'
-  sha256 '9626d60ac5fbe390d9a623c430378fd3e81264bd07338e292093446443766774'
-  url 'https://raw.githubusercontent.com/tomologic/utilities/36a28f43ef364d66007c2314dde9fa9c48deb52f/travis-ci-bumpminor.sh'
+
+  version '0.1.2'
+  sha256 'eaa648684c3769c117723918ddd52b6de477d53d35c104e68f17df7cf3c1a807'
+  url 'https://raw.githubusercontent.com/tomologic/utilities/5e48708acb5ca6c98edad246ae9774c289a35835/travis-ci-bumpminor.sh'
+
   depends_on 'tomologic/tap/semver-sh' => :recommended
 
   def install
