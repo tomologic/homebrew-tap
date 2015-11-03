c650e627bfdcf7be069cc9483a0b25423c9e19ce
Carl Loa Odin <loa@tomologic.com>
2015-11-03 13:52:56 +0100

    Add dependency semver-sh in travis-ci-bumpminor-sh

--- travis-ci-bumpminor-sh.rb
+++ travis-ci-bumpminor-sh.rb
@@ -1,9 +1,10 @@
 class TravisCiBumpminorSh < Formula
   desc 'travis-ci-bumpminor.sh'
   homepage 'https://github.com/tomologic/utilities'
-  version '0.1.0'
+  version '0.1.1'
   sha256 '9626d60ac5fbe390d9a623c430378fd3e81264bd07338e292093446443766774'
   url 'https://raw.githubusercontent.com/tomologic/utilities/36a28f43ef364d66007c2314dde9fa9c48deb52f/travis-ci-bumpminor.sh'
+  depends_on 'tomologic/tap/semver-sh' => :recommended
 
   def install
     bin.install 'travis-ci-bumpminor.sh'
