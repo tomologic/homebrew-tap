867686b006c2a822d57093a770f0aa522adf3c07
Stanislav Blokhin <stanislav.blokhin@tomologic.com>
2016-06-28 16:00:35 +0200

    Upgrade Wrench to v0.38.0

--- wrench.rb
+++ wrench.rb
@@ -1,14 +1,14 @@
 class Wrench < Formula
   desc 'Wrench build / test util'
   homepage 'https://github.com/tomologic/wrench'
-  version '0.37.0'
+  version '0.38.0'
 
   if OS.mac?
     os = 'darwin'
-    sha256 '213103dd47b82c9cd4483331da7ac73d91522583b3f3aedd7c42c22004d14973'
+    sha256 'a6c379be3cc4d17f40c06708496e5378477bc3d9b671211e51fa631dc17163c8'
   elsif OS.linux?
     os = 'linux'
-    sha256 '53e21d7c16fddbfeb7a01ccfe099c8db0c98b3bd6cd8d7a74dce68c8fbb63102'
+    sha256 '78e5b09a247554baf245eb723672b016efd9346f3ce5e329564d408634a2dbf6'
   end
 
   url "https://github.com/tomologic/wrench/releases/download/v#{version}/wrench-#{version}-#{os}-amd64"
